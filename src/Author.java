d452c36c3b158c6063e225ca17c79dec99d11851
NN <nik_rink@mail.ru>
2023-07-05 00:39:06 +0300

    Сделал ДЗ-12,задачи 1-1, поместил в hw12 ветку

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String name;
+    private String family;
+
+    public  Author(String name, String family){
+        this.name = name;
+        this.family = family;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getFamily(){
+        return this.family;
+    }
+}
